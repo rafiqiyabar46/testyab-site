a1d0f403e332487f452b74d80c8362cd0bf516c1
rafiqiyabar46 <rafiqiyabar46@gmail.com>
2026-04-23 17:09:02 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function showMessage() {
-    alert("Hello! Thanks for visiting my webpage.");
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if (username === "yabar" && password === "password") {
+        window.location.href = "welcome.html";  // Redirect to the new page
+    } else {
+        document.getElementById("message").innerText = "Invalid credentials!";
+        document.getElementById("message").style.color = "red";
+    }
 }
